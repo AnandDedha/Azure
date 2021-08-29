eabee4e0028f00f3490df47029fb8097a24a5ef2
Anand Kumar <ananddedha@gmail.com>
2021-08-29 15:23:39 -0400

    Update data_source_table.sql

--- Incremental-load/data_source_table.sql
+++ Incremental-load/data_source_table.sql
@@ -1,9 +1,26 @@
+
 create table data_source_table
  (
- 	PersonID int,
- 	Name varchar(255),
- 	LastModifytime datetime
+ 	[PersonID] int,
+ 	[Name] varchar(255),
+ 	[LastModifytime] datetime
  );
+
+ INSERT INTO data_source_table ([PersonID], [Name], [LastModifytime])
+ VALUES (1, 'aaaa','9/1/2017 12:56:00 AM');
+ 
+ INSERT INTO data_source_table ([PersonID], [Name], [LastModifytime])
+ VALUES (2, 'bbbb','9/2/2017 5:23:00 AM');
+ 
+ INSERT INTO data_source_table ([PersonID], [Name], [LastModifytime])
+ VALUES (3, 'cccc','9/3/2017 2:36:00 AM');
+
+ INSERT INTO data_source_table ([PersonID], [Name], [LastModifytime])
+ VALUES (4, 'dddd','9/4/2017 3:21:00 AM');
+ 
+ INSERT INTO data_source_table ([PersonID], [Name], [LastModifytime])
+ VALUES (5, 'eeee','9/5/2017 8:06:00 AM');
+
 
  INSERT INTO data_source_table
  (PersonID, Name, LastModifytime)
