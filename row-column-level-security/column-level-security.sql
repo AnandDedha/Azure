ddd3b131d4c82804db34dedbc686cd1be7f0376b
Anand Kumar <ananddedha@gmail.com>
2022-03-20 14:44:02 -0400

    Update column-level-security.sql

--- row-column-level-security/column-level-security.sql
+++ row-column-level-security/column-level-security.sql
@@ -4,8 +4,7 @@ GO
 
 -- Create a table
 CREATE TABLE sales2.Membership
-  (MemberID int IDENTITY,
-   FirstName varchar(100) NULL,
+  (FirstName varchar(100) NULL,
    SIN char(9) NOT NULL,
    LastName varchar(100) NOT NULL,
    Phone varchar(12) NULL,
@@ -21,7 +20,7 @@ INSERT INTO sales2.Membership VALUES   ('San','333333333' ,'Dp', '888.123.4567',
  CREATE USER TestUser WITHOUT LOGIN; 
  
  -- Allow TestUser to access all columns except for the SSN column, which has the sensitive data
- GRANT SELECT ON Membership(MemberID, FirstName, LastName, Phone, Email) TO TestUser;
+ GRANT SELECT ON Membership(FirstName, LastName, Phone, Email) TO TestUser;
  
 -- impersonate for testing:
 EXECUTE AS USER = 'TestUser';   
